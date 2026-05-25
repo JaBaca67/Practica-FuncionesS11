14ccfc5f0e54db6b6f53be1a70eb4fcc49734365
ceratiwong <michaelwong@uamv.edu.ni>
2026-05-25 10:57:08 -0600

    feat: validar_edades_caso6

---
+++ validar_edades/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Introduce tu edad: ");
+string entrada = Console.ReadLine();
+
+if (ValidarEdad(entrada))
+{
+    Console.WriteLine("¡Edad válida!");
+}
+else
+{
+    Console.WriteLine("Edad no válida. Debe ser un número entero positivo.");
+}
+
+bool ValidarEdad(string texto)
+{
+    if (int.TryParse(texto, out int edad))
+    {
+        return edad >= 0 && edad <= 100;
+    }
+    return false;
+}
